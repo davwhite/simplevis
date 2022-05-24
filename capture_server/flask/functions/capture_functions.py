db8700b10649fab0751d69849bb8bc247514fc49
david white <davwhite@davwhite.com>
2022-05-24 11:51:15 -0400

    Cleaned up capture server

--- capture_server/flask/functions/capture_functions.py
+++ capture_server/flask/functions/capture_functions.py
@@ -3,15 +3,6 @@ import os
 import cv2
 from datetime import datetime
 
-# config = {
-#   'user': os.environ.get('DB_USER'),
-#   'password': os.environ.get('DB_PWD'),
-#   'host': os.environ.get('DB_HOST'),
-#   'database': os.environ.get('DB_NAME'),
-#   'raise_on_warnings': True
-# }
-
-
 def test():
   jsondata = ['this is a test']
   return jsondata
@@ -42,19 +33,3 @@ def capture():
   # print(output)
   jsondata = ['captured']
   return jsondata
-
-
-# def panel_statements(panel_id, statement_type, speaker):
-#   if panel_id != "N/A":
-#     query = ("select statement,speaker,statement_time,statement_type from panel_statements where panel_id = '" + panel_id + "'")
-#   elif statement_type != "N/A":
-#     query = ("select a.panel_id,b.panel_title,a.speaker,a.statement,a.statement_time,a.statement_type from panel_statements a, panel_details b where a.panel_id=b.panel_id and a.statement_type = '" + statement_type + "'")
-#   elif speaker != "N/A":
-#     query = ("select a.panel_id,b.panel_title,a.speaker,a.statement,a.statement_time,a.statement_type from panel_statements a, panel_details b where a.panel_id=b.panel_id and a.speaker = '" + speaker + "'")
-#   jsondata = get_query_results(query)
-#   return jsondata
-
-# def panel_search(searchstring):
-#   query = ("select a.panel_id, b.panel_title, a.statement, a.speaker, a.statement_time, a.statement_type from panel_statements a, panel_details b where a.panel_id=b.panel_id and match(a.statement) against ('" + searchstring +"' in natural language mode)")
-#   jsondata = get_query_results(query)
-#   return jsondata
